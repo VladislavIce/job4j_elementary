1955f585a6830f1d07fa57a402f7587691010e75
Vladi <nilindalv@yandex.ru>
2023-11-16 13:42:08 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Длина массива: " + ages.length);
         System.out.println("Длина массива: " + surnames.length);
         System.out.println("Длина массива: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Владислав Ильин";
+        names[1] = "Николай Ильин";
+        names[2] = "Петр Ильин";
+        names[3] = "Иван Ильин";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
